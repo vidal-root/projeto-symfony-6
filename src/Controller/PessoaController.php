a9823b8a358fe191145d5a6346a96effb07f13f9
Luis Vidal <vidal.desenv@gmail.com>
2024-01-15 19:59:00 -0300

    utilizando o parent do twig

--- src/Controller/PessoaController.php
+++ src/Controller/PessoaController.php
@@ -10,11 +10,36 @@ class PessoaController extends AbstractController{
 
     #[Route('/pessoa')]
     public function index(): Response {
-        return new Response('Ola controller pessoa');
+        
+        $pessoas = [
+            [
+                'nome' => 'João',
+                'telefone' => '123-456-7890',
+            ],
+            [
+                'nome' => 'Maria',
+                'telefone' => '987-654-3210',
+            ],
+            [
+                'nome' => 'Pedro',
+                'telefone' => '555-123-4567',
+            ],
+            [
+                'nome' => 'Ana',
+                'telefone' => '777-888-9999',
+            ],
+            [
+                'nome' => 'Carlos',
+                'telefone' => '111-222-3333',
+            ],
+        ];
+
+        return $this->render('pessoa/homepage.html.twig', ['pessoas' => $pessoas]);
     }
 
     #[Route('/pessoa/info/{nome_pessoa}')]
     public function info(string $nome_pessoa): Response {
+        dump(['ola']);
         return $this->render('pessoa/pessoa.html.twig', ['nome_pessoa' => $nome_pessoa]);
     }
 
