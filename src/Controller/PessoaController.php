1456130183c00654060d380a8faee4d3efc4fcb8
Luis Vidal <vidal.desenv@gmail.com>
2024-01-15 10:22:18 -0300

    Primeira rota redendo template adicionada

--- src/Controller/PessoaController.php
+++ src/Controller/PessoaController.php
@@ -4,12 +4,18 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class PessoaController{
+class PessoaController extends AbstractController{
 
     #[Route('/pessoa')]
     public function index(): Response {
         return new Response('Ola controller pessoa');
+    }
+
+    #[Route('/pessoa/info/{nome_pessoa}')]
+    public function info(string $nome_pessoa): Response {
+        return $this->render('pessoa/pessoa.html.twig', ['nome_pessoa' => $nome_pessoa]);
     }
 
 }
