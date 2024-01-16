898c089f55dbd1cc1d99efa09cd798dd4456b72d
Luis Vidal <vidal.desenv@gmail.com>
2024-01-16 00:25:24 -0300

    adicionando o debug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController{
 
-    #[Route('/home')]
+    #[Route('/home', name: 'app_nome')]
     public function new(): Response{
 
         return $this->render('home/home.html.twig');
